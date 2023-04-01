85160796e5aefa01fce583557e36c1723e9772c1
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-04-01 21:29:08 +0100

    Setup eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Enable ESLint during production builds
+    ignoreDuringBuilds: false,
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useEffect } from 'react';
 import Layout from '@/components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
+    import('bootstrap/dist/js/bootstrap');
   }, []);
 
-  return (<Layout>
-    <Component {...pageProps} />
-  </Layout>)
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
