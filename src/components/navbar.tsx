d97dd7f60155470ef60895e1bce58e746d7b480f
Pavaruth Buranakitsin <2646802b@student.gla.ac.uk>
2023-04-02 00:58:02 +0100

    Remade navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,73 +1,33 @@
-export default function Navbar() {
-  return (
-    <nav
-      className="navbar navbar-expand-md navbar-dark bg-dark "
-      aria-label="Eighth navbar example"
-    >
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          Container
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarsExample07"
-          aria-controls="navbarsExample07"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarsExample07"
+export default function CollapsibleNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark" sticky='top' className='pt-4 pb-4'>
+      <Container>
+        <Navbar.Brand href="">PavaruthB</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
         >
-          <ul className="navbar-nav mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="#scrollspyHeading1">
-                First
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+          <Nav>
+            <Nav.Link
+              onClick={() => {
+                console.log('clicked');
+              }}
+            >
+              About me
+            </Nav.Link>
+            <Nav.Link>Experience</Nav.Link>
+            <Nav.Link>Projects</Nav.Link>
+            <Nav.Link>Contact</Nav.Link>
+          </Nav>
+          <Button className='ms-md-2 mt-md-0 mt-2' variant="outline-primary">Resume</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
